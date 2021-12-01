d9e6971c46e012469053234ced0d06f1d58236a1
s3819293 <s3819293@rmit.edu.vn>
2021-12-01 09:57:26 +0700

    manager getter and setter

--- ItemManager.h
+++ ItemManager.h
@@ -8,8 +8,28 @@
 class ItemManager
 {
 private:
-
+	vector<Item*> itemList;
+	int noOfItem;
+	string itemFile; //file name
 public:
+	//Constructor
+	ItemManager();
+	ItemManager(string itemFile); //Use to read from item file
 
+	//Destructor
+	~ItemManager() {};
+
+	//Getter
+	vector<Item*> getItemList();
+	int getNoOfItem();
+	string getItemFile();
+
+	//Setter
+	void setItemList(vector<Item*> itemList);
+	void setNoOfItem(int noOfItem);
+	void setItemFile(string itemFile);
+
+	//Other function
+	void print();
 };
 
