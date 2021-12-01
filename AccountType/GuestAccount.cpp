bee83f884859094621126c3ad63be66961bf2e41
s3819293 <s3819293@rmit.edu.vn>
2021-12-01 13:29:52 +0700

    account cons get set

--- AccountType/GuestAccount.cpp
+++ AccountType/GuestAccount.cpp
@@ -1,1 +1,33 @@
 #include "GuestAccount.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//Constructor
+GuestAccount::GuestAccount() {
+
+}
+GuestAccount::GuestAccount(string id, string name, string address, string phone, int noOfRentals) :
+	Account(id, name, address, phone, noOfRentals) {
+	this->totalBorrowItem = 0;
+	this->totalReturnItem = 0;
+}
+
+//Getter
+int GuestAccount::getTotalBorrowItem() {
+	return this->totalBorrowItem;
+}
+int GuestAccount::getTotalReturnItem() {
+	return this->totalReturnItem;
+}
+
+//Setter
+void GuestAccount::setTotalBorrowItem(int totalBorrowItem) {
+	this->totalBorrowItem = totalBorrowItem;
+}
+void GuestAccount::setTotalReturnItem(int totalReturnItem) {
+	this->totalReturnItem = totalReturnItem;
+}
--- AccountType/RegularAccount.cpp
+++ AccountType/RegularAccount.cpp
@@ -1,1 +1,33 @@
 #include "RegularAccount.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//Constructor
+RegularAccount::RegularAccount() {
+
+}
+RegularAccount::RegularAccount(string id, string name, string address, string phone, int noOfRentals) :
+	Account(id, name, address, phone, noOfRentals) {
+	this->totalBorrowItem = 0;
+	this->totalReturnItem = 0;
+}
+
+//Getter
+int RegularAccount::getTotalBorrowItem() {
+	return this->totalBorrowItem;
+}
+int RegularAccount::getTotalReturnItem() {
+	return this->totalReturnItem;
+}
+
+//Setter
+void RegularAccount::setTotalBorrowItem(int totalBorrowItem) {
+	this->totalBorrowItem = totalBorrowItem;
+}
+void RegularAccount::setTotalReturnItem(int totalReturnItem) {
+	this->totalReturnItem = totalReturnItem;
+}
