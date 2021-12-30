17addce55fe0a009757d768c65cb17eec685ba45
namtoptall <73808933+namtoptall@users.noreply.github.com>
2021-12-30 23:23:37 +0700

    namnp2002

--- ItemManager.cpp
+++ ItemManager.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
@@ -115,6 +116,7 @@ string ItemManager::getItemFile() {
 }
 
 //Setter
+
 void ItemManager::setItemList(vector<Item*> itemList) {
 	this->itemList = itemList;
 }
@@ -147,41 +149,131 @@ bool ItemManager::updateItem(string id) {
 bool ItemManager::deleteItem(string id) {
 	return true;
 }
+bool mycomp(string a, string b){
+	//returns 1 if string a is alphabetically 
+	//less than string b
+	//quite similar to strcmp operation
+	return a<b;
+}
+
+vector<string> alphabeticallySort(vector<string> a){
+	int n=a.size();
+	//mycomp function is the defined function which 
+	//sorts the strings in alphabetical order
+	sort(a.begin(),a.end(),mycomp);
+	return a;
+}
 
 void ItemManager::displaySortedItemTitle() {
-	cout << "sort by title" << endl;
+	vector<string> itemNameList;
+	for (Item* item : this->getItemList()) 
+	{
+		itemNameList.push_back(item->getTitle());
+	}
+	alphabeticallySort(itemNameList);
+	cout << "The item list is: ";
+	for (auto itemName: itemNameList){
+		cout << itemName;
+	}
+	cout << endl;
 }
+
 void ItemManager::displaySortedItemID() {
-	cout << "sort by id" << endl;
+	vector<string> itemIDList;
+	vector<int> itemIDNumber;
+	for (Item* item : this->getItemList()) {
+		itemIDList.push_back(item->getId());
+	}
+	
+	for (string ID: itemIDList){
+		string IDString = ID.substr(1,3)+ID.substr(5, 8);
+		int IDInt;
+		if (sscanf(IDString.c_str(), "%d", &IDInt) !=1)
+			itemIDNumber.push_back(IDInt);
+	}
+	sort(itemIDNumber.begin(), itemIDNumber.end());
+	for (auto ID: itemIDNumber)
+	{
+	cout << "I" << to_string(ID)[0] << to_string(ID)[1] << to_string(ID)[2]
+    << "-" << to_string(ID)[3] << to_string(ID)[4] << to_string(ID)[5] << to_string(ID)[6];
+	}
+	
 }
-void ItemManager::getAllNoStock() {
+void ItemManager::getAllNoStock() 
+{
 	cout << "sort by no of stock = 0" << endl;
 }
-void ItemManager::searchItem(string title) {
-	cout << "search by title" << endl;
-}
-void ItemManager::searchItem(char* id) {
-	cout << "search by id" << endl;
+
+
+void ItemManager::searchItem(string title) 
+{
+	vector <string> titleSearchResult;
+	for (Item* item : this->getItemList()) 
+	{
+		if (item->getTitle().find(title))
+		{
+			titleSearchResult.push_back(item->getTitle());
+		}
+	}
+	cout << "The title search result is: " << endl;
+	for (auto titleResult: titleSearchResult)
+	{
+		cout << titleResult << endl;
+	}
+	
 }
 
-bool ItemManager::saveToFile() {
+void ItemManager::searchItem(char* id) 
+{
+	vector<string> itemIDList;
+	vector<string> itemIDNumber;
+	vector<string> IDSearchResult;
+	
+	for (Item* item : this->getItemList()) 
+	{
+		itemIDList.push_back(item->getId());
+	}
+	for (auto ID: itemIDList){
+		itemIDNumber.push_back(ID.substr(1,3)+ID.substr(5, 8));
+	}
+	
+	for (auto ID: itemIDNumber){
+		if (ID.find(id)){
+			IDSearchResult.push_back("I"+ ID.substr(0,2) + "-" +ID.substr(3,6));	
+		}
+	}
+	cout << "The ID search result is: "<<endl;
+	for (auto ID: IDSearchResult){
+		cout << ID << endl;
+	}
+}
+
+
+bool ItemManager::saveToFile() 
+{
 	ofstream outStream(this->itemFile);
 
-	for (Item* item : this->itemList) {
+	for (Item* item : this->itemList) 
+	{
 		outStream << item->toString() << endl;
 	}
 	return true;
 }
 
-void ItemManager::displayAll() {
-	for (Item* item : this->getItemList()) {
+void ItemManager::displayAll() 
+{
+	for (Item* item : this->getItemList()) 
+	{
 		item->print();
 	}
 }
 
-void ItemManager::getItemFromRental(string itemID) {
-	for (Item* item : this->itemList) {
-		if (item->getId() == itemID) {
+void ItemManager::getItemFromRental(string itemID) 
+{
+	for (Item* item : this->itemList) 
+	{
+		if (item->getId() == itemID) 
+		{
 			item->print();
 		}
 	}
