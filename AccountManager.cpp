05ab471af81fbc6ac27f1eef7c31973223d52641
namtoptall <73808933+namtoptall@users.noreply.github.com>
2022-01-10 13:42:17 +0700

    Update AccountManager.cpp

--- AccountManager.cpp
+++ AccountManager.cpp
@@ -188,97 +188,226 @@ bool AccountManager::promoteAccount(string id) {
 	return matched;
 }
 
+//Function to add a new account of type Guest
 bool AccountManager::addAccount() {
-	string input;
-	string name, address, phone, type;
-	
-	this->noOfAccount++;
+	string input; //string for geting the input
+	string name, address, phone, type; //string data fields to create a new account
 
+	this->noOfAccount++; //get the size of the vector (number of account) and increase by 1 to create a new id
+
+	//Set the id in the format Cxxx (where x is number from 0 to 9)
 	ostringstream streamID;
 	streamID << "C" << setw(3) << setfill('0') << this->noOfAccount;
 	string id = streamID.str();
 
+	//While true loop that ask for the name input
 	while (true) {
-		cout << "Please enter your name: ";
+		cout << "\nPlease enter your name: ";
 		getline(cin, input);
-		
+
 		//trim string
 		input.erase(input.find_last_not_of(" ") + 1);
 		input.erase(0, input.find_first_not_of(" "));
 
-		if (!input.empty()) {
-			name = input;
+		if (!input.empty()) { //Check if the input is empty or not
+			name = input; //if not empty then set name to be input
 			break;
+		}
+		else {
+			cerr << "The field cannot be empty." << endl; //printout error message if field empty
 		}
 	}
 
+	//While true loop that ask for the address input
 	while (true) {
-		cout << "Please enter your address: ";
+		cout << "\nPlease enter your address: ";
 		getline(cin, input);
-		
+
 		//trim string
 		input.erase(input.find_last_not_of(" ") + 1);
 		input.erase(0, input.find_first_not_of(" "));
 
-		if (!input.empty()) {
-			address = input;
+		if (!input.empty()) { //Check if the input is empty or not
+			address = input;//if not empty then set address to be input
 			break;
+		}
+		else {
+			cerr << "The field cannot be empty." << endl; //printout error message if field empty
 		}
 	}
 
+	//While true loop that ask for the phone number input
 	while (true) {
-		cout << "Please enter your phone number: ";
+		cout << "\nPlease enter your phone number: ";
 		getline(cin, input);
 
 		//trim string
 		input.erase(input.find_last_not_of(" ") + 1);
 		input.erase(0, input.find_first_not_of(" "));
 
-		bool matched = true;
+		bool matched = true; //boolean value to check the phone number input
 
-		if (!input.empty()) {
-			for (int i = 0; i < input.length(); i++) {
-				if (input[i] < 32 || input[i] > 57) {
-					matched = false;
+		if (!input.empty()) { //Check if the input is empty or not
+			for (int i = 0; i < input.length(); i++) { //chech if the input contain character is number
+				if (input[i] < 48 || input[i] > 57) {
+					matched = false; //if countain, set the boolean value to be false and break the loop
 					break;
 				}
-				//cout << input[i];
 			}
 
+			//Check if the input is correct
 			if (matched == true) {
-				phone = input;
+				phone = input; //if correct then set phone to be input
 				break;
 			}
+			else {
+				cerr << "This field can only have numbers." << endl; //printout error message if field contain anything but number or special character (+ or -)
+			}
+		}
+		else {
+			cerr << "The field cannot be empty." << endl; //printout error message if field empty
 		}
 	}
 
+	//Create a new Guest account after getting all the require fields
 	Account* newAccount = new GuestAccount(id, name, address, phone, 0, "Guest");
-	this->accountList.push_back(newAccount);
-	newAccount->print();
+	this->accountList.push_back(newAccount); //add the account to the vector
+	cout << "\nThe account " << newAccount->getId() << " with the information: \n\tName: " << newAccount->getName() << "\n\tAddress: " << newAccount->getAddress() << "\n\tPhone: " << newAccount->getPhone() << "\nhas been successfully added to the system\n";
 
-	if (type == "Guest") {
-		Account* newAccount = new GuestAccount(id, name, address, phone, 0, type);
-		this->accountList.push_back(newAccount);
-		return true;
-	}
-	else if (type == "Regular") {
-		Account* newAccount = new GuestAccount(id, name, address, phone, 0, type);
-		this->accountList.push_back(newAccount);
-		return true;
-	}
-	else if (type == "VIP") {
-		Account* newAccount = new GuestAccount(id, name, address, phone, 0, type);
-		this->accountList.push_back(newAccount);
-		return true;
-	}
-	else {
-		cerr << "error message" << endl;
-	}
 	return true;
 }
 bool AccountManager::updateAccount(string id) {
+
+	Account* updateAcc = nullptr; //create a temporary account 
+	string input, field, oldInfo;
+
+	bool accountFound = false; //boolean value to check if the account match the id input exist
+
+	//For loop to find the account that match the id input
+	for (Account* acc : this->accountList) {
+		if (acc->getId() == id) { //if found, then set the account to be the temporary acc for update
+			updateAcc = acc;
+			accountFound = true;
+			break;
+		}
+	}
+
+	//If no account that match the id is found then end the function and printout a message to inform the user
+	if (!accountFound) {
+		cout << "\nThe account does not exsist or have been deleted" << endl;
+		return false;
+	}
+
+	string requestField = "\nWhich field you want to update on the Account " + updateAcc->getId()
+		+ "\n1. Name"
+		+ "\n2. Address"
+		+ "\n3. Phone"
+		+ "\n4. Exit";
+
+	//If the account is found the start a while loop that will ask user what field they want to update
+	while (true) {
+		cout << requestField << endl;
+
+		getline(cin, input); //get the input base for update field
+
+		//trim string
+		input.erase(input.find_last_not_of(" ") + 1);
+		input.erase(0, input.find_first_not_of(" "));
+
+		if (input == "1") { //input = 1 --> update customer name
+			oldInfo = updateAcc->getName();
+
+			//While true loop that ask for the name input
+			while (true) {
+				cout << "\nPlease enter the name you want to change to: ";
+				getline(cin, field);
+
+				//trim string
+				field.erase(field.find_last_not_of(" ") + 1);
+				field.erase(0, field.find_first_not_of(" "));
+
+				if (!field.empty()) { //Check if the input is empty or not
+					updateAcc->setName(field); //if not empty then set name to be input for new name and prinout appropriate message
+					cout << "\nThe name of the account " << updateAcc->getId() << " had been succesfully change from " << oldInfo << " to " << updateAcc->getName() << endl;
+					break;
+				}
+				else {
+					cerr << "The field cannot be empty." << endl; //printout error message if field empty
+				}
+			}
+		}
+		else if (input == "2") { //input = 2 --> update customer address
+			oldInfo = updateAcc->getAddress();
+
+			//While true loop that ask for the address input
+			while (true) {
+				cout << "\nPlease enter the address you want to change to: ";
+				getline(cin, field);
+
+				//trim string
+				field.erase(field.find_last_not_of(" ") + 1);
+				field.erase(0, field.find_first_not_of(" "));
+
+				if (!field.empty()) { //Check if the input is empty or not
+					updateAcc->setAddress(field); //if not empty then set address to be input for new address and prinout appropriate message
+					cout << "\nThe address of the account " << updateAcc->getId() << " had been succesfully change from " << oldInfo << " to " << updateAcc->getAddress() << endl;
+					break;
+				}
+				else {
+					cerr << "The field cannot be empty." << endl; //printout error message if field empty
+				}
+			}
+		}
+		else if (input == "3") { //input = 2 --> update customer phone
+			oldInfo = updateAcc->getPhone();
+
+			//While true loop that ask for the phone number input
+			while (true) {
+				cout << "\nPlease enter the phone number you want to change to: ";
+				getline(cin, field);
+
+				//trim string
+				field.erase(field.find_last_not_of(" ") + 1);
+				field.erase(0, field.find_first_not_of(" "));
+
+				bool matched = true; //boolean value to check the phone number input
+
+				if (!input.empty()) { //Check if the input is empty or not
+					for (int i = 0; i < input.length(); i++) { //chech if the input contain character is number
+						if (input[i] < 48 || input[i] > 57) {
+							matched = false; //if countain, set the boolean value to be false and break the loop
+							break;
+						}
+					}
+
+					////Check if the input is correct
+					if (matched == true) {
+						updateAcc->setPhone(field); //if not empty then set phone number to be input for new address and prinout appropriate message
+						cout << "\nThe phone number of the account " << updateAcc->getId() << " had been succesfully change from " << oldInfo << " to " << updateAcc->getPhone() << endl;
+						break;
+					}
+					else {
+						cerr << "This field can only have numbers." << endl; //printout error message if field contain anything but number
+					}
+				}
+				else {
+					cerr << "The field cannot be empty." << endl; //printout error message if field empty
+				}
+			}
+		}
+		else if (input == "4") { //If user want to stop update the fields
+			cout << "\nExit\n" << endl;
+			break; //Break the loop
+		}
+		else {
+			cerr << "\nInvalid input." << endl;
+		}
+	}
+
 	return true;
 }
+
+
 
 void AccountManager::displaySortedAccountName()
 {
@@ -376,12 +505,15 @@ void AccountManager::searchAccount(char *id)
 }
 
 
+//Function to save the accountList to a text file
 bool AccountManager::saveToFile() {
-	ofstream outStream(this->customerFile);
+	ofstream outStream(this->customerFile); //open the file used to get the information
 
+	//loop through the array and add the account info to the appropriate format
 	for (Account* account : this->accountList) {
 		outStream << account->toString() << endl;
-		for (string itemID: account->getListOfRentals()) {
+		//for each accout, loop through their list of rental and add the item list to the file
+		for (string itemID : account->getListOfRentals()) {
 			outStream << itemID << endl;
 		}
 	}
